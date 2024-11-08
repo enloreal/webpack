29a74c4a8ed2a5a5b3c2dd6875f2b8be10fd23d6
enloreal <arozarg@mail.ru>
2024-11-08 16:06:52 +0300

    changed problem with webpack.dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,43 +3,9 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'source-map',  // для удобной отладки
+  devtool: 'source-map',
   devServer: {
     static: './dist',
     hot: true,
   },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.css$/,  
-        use: ['style-loader', 'css-loader'], 
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './src/template.html'),
-      filename: 'index.html',
-      title: 'React + TypeScript + Webpack Setup',
-    }),
-  ],
 });
